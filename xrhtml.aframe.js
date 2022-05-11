1ace5af2bdf09f539f5adcddebae5bd0fe3f0924
Leon <leonvankammen@gmail.com>
2022-05-11 17:49:53 +0200

    work in progress [might break]

--- xrhtml.aframe.js
+++ xrhtml.aframe.js
@@ -15,15 +15,26 @@ AFRAME.registerComponent('xrhtml', {
     let opts = {
       name: this.data.name || "xrhtml"+String(Math.random()).substr(3, 9), 
       size: this.data.size || [500, 200], 
-      class: this.data.class || "xrhtml", 
+      class: this.data.name || this.data.class || "xrhtml", 
       scene
     }
     if( this.data.url ) opts.url = this.data.url
     if( this.el.innerHTML ) opts.html = this.el.innerHTML;
-    this.app = new XRHTML(opts);
-    if( this.data.position ) this.app.position = this.el.object3D.position.clone()
-    if( this.data.rotation ) this.app.rotation = this.el.object3D.rotation.clone()
+    this.app = this.el.app = new XRHTML(opts);
     scene.object3D.add(this.app)
+    this.update()
   }, 
+
+  update: function(){
+    this.app.position.set( this.el.object3D.position.x, 
+                           this.el.object3D.position.y, 
+                           this.el.object3D.position.z )
+    this.app.rotation.set( this.el.object3D.rotation.x, 
+                           this.el.object3D.rotation.y, 
+                           this.el.object3D.rotation.z )
+    this.app.scale.set( this.el.object3D.scale.x, 
+                           this.el.object3D.scale.y, 
+                           this.el.object3D.scale.z )
+  }
 
 });
